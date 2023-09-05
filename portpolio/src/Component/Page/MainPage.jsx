c497282fd301bae673afe959f75479deddeeb40b
jaeochoii <jaeochoiii0716@inha.edu>
2023-09-05 14:12:15 +0900

    fix: MainPage MainText 수정

--- portpolio/src/Component/Page/MainPage.jsx
+++ portpolio/src/Component/Page/MainPage.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import mainImage from "../../Assets/MainImage.jpg";
+import mainImage from "../../Assets/Image.png";
 const MainBodyWrapper = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
   background-color: black;
 `;
-const MainBodyImage = styled.img`
+
+const MainImageWrapper = styled.div`
+  position: relative;
   width: 100vw;
   height: 100vh;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainBodyImage = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0.8;
+`;
+
+const MainBodyText = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap");
+  font-family: "Do Hyeon", sans-serif;
+  font-size: 46px;
+  color: white;
+  margin-bottom: 20px;
   opacity: 0.8;
 `;
 
 const CategoryBodyWrapper = styled.div`
-  width: 380px;
+  width: 350px;
   height: 100vh;
   background-color: rgb(32, 34, 36);
 `;
@@ -24,7 +45,13 @@ const MainPage = () => {
     <>
       <MainBodyWrapper>
         <CategoryBodyWrapper />
-        <MainBodyImage src={mainImage} />
+        <MainImageWrapper>
+          <MainBodyImage src={mainImage}></MainBodyImage>
+          <MainBodyText>어제보다 더 성장한 오늘을 꿈꾸는 개발자</MainBodyText>
+          <MainBodyText>
+            <span style={{ fontSize: 88 }}>최재오</span>입니다.
+          </MainBodyText>
+        </MainImageWrapper>
       </MainBodyWrapper>
     </>
   );
